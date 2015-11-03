87625827a1f943e77c7e4f02c5acfa9ed2801936
Jun Kai <kuhn96@gmail.com>
2015-11-03 13:03:13 +0800

    Remove devise user

--- db/schema.rb
+++ db/schema.rb
@@ -83,29 +83,5 @@ ActiveRecord::Schema.define(version: 20151102133825) do
   add_index "subscriptions", ["shops_id"], name: "index_subscriptions_on_shops_id", using: :btree
   add_index "subscriptions", ["users_id"], name: "index_subscriptions_on_users_id", using: :btree
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "name",                   limit: 255
-    t.integer  "status",                 limit: 4
-    t.string   "reset_password_token",   limit: 255
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
   add_foreign_key "shops", "owners"
 end
