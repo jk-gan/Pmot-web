f6846b4f0eebb7f56aee7d8d6d564d7c2de6f528
Jun Kai <kuhn96@gmail.com>
2015-11-03 14:36:59 +0800

    Added secure password to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :status)
+    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation)
   end
 end
