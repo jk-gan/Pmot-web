187e45c8d8609915a4e0649b75e734dd42a5ad29
Jun Kai <kuhn96@gmail.com>
2015-11-11 01:42:38 +0800

    Modified the qr code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
      if current_owner.admin?
        redirect_to admin_root_path
      else
-       @qr = RQRCode::QRCode.new(current_owner.email).to_img.resize(200, 200).to_data_url
+       @qr = RQRCode::QRCode.new("Pmot@" + current_owner.shop.identity).to_img.resize(200, 200).to_data_url
      end
    else
      redirect_to new_owner_session_path
