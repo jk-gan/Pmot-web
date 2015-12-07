3ba99793f3b374c8239b8693892325b4dbfac783
Jun Kai <kuhn96@gmail.com>
2015-12-08 00:49:45 +0800

    Remove confirmation for owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -9,7 +9,7 @@ class Owner < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 
   accepts_nested_attributes_for :shop, allow_destroy: true
 
